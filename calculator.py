7465bac174d63d40086b93ced9f708cd5a579fc9
shadow20254 <bodekarmohit@gmail.com>
2025-06-13 12:57:15 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+a=int(input("Enter a number "))
+b=int(input("Enter another number "))
+c=input("Enter the operator ( +,-,*,/ ) ")
+
+if c =='+':
+    print(a+b)
+elif c =='-':
+    print(a-b)
+elif c =='*':
+    print(a*b)
+elif c =='/':
+    print(a/b)
+else:
+    print("none")
+
+print('thankyou')
